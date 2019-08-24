c814034fcb50f629835c98a508524f4a799d81bb
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2019-08-24 09:07:22 +0200

    Adding two arguments and an option

--- commands/GetShaCommand.php
+++ commands/GetShaCommand.php
@@ -4,15 +4,20 @@ namespace Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GetShaCommand extends Command
 {
+    protected static $defaultName = 'getsha';
+
     protected function configure()
     {
         $this->setName('getsha');
         $this->setDescription('Prints the SHA1 of the last commit to the screen');
-        $this->addArgument('service');
+        $this->addArgument('repo', null, InputOption::VALUE_REQUIRED);
+        $this->addArgument('branch', null, InputOption::VALUE_REQUIRED);
+        $this->addOption('service', null, InputOption::VALUE_OPTIONAL);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
--- getsha.php
+++ getsha.php
@@ -6,6 +6,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
+$getShaCommand = new Commands\GetShaCommand();
+
 $script = new Application();
-$script->add(new Commands\GetShaCommand());
+$script->add($getShaCommand);
+/**
+ * Unfortunately, we cannot set GetShaCommand as default, because according to the Symfony docs, arguments would be then ignored.
+ * So instead, we seem to have to add 'getsha' as a first argument.
+ * @see https://symfony.com/doc/current/components/console/changing_default_command.html
+ */
+//$script->setDefaultCommand($getShaCommand->getName());
 $script->run();

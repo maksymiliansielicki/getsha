3bf3a82512ccc0c36eb5c3a1e7f67a66d8030ef9
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2019-08-24 09:47:07 +0200

    Validate input

--- commands/GetShaCommand.php
+++ commands/GetShaCommand.php
@@ -15,6 +15,7 @@ class GetShaCommand extends Command
     {
         $this->setName('getsha');
         $this->setDescription('Prints the SHA1 of the last commit to the screen');
+        $this->setHelp('Supported websites: GitHub. Future support will include Bitbucket and GitLab');
         $this->addArgument('repo', null, InputOption::VALUE_REQUIRED);
         $this->addArgument('branch', null, InputOption::VALUE_REQUIRED);
         $this->addOption('service', null, InputOption::VALUE_OPTIONAL);
@@ -22,6 +23,8 @@ class GetShaCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->validate($input);
+
         $output->write($this->getSha());
     }
 
@@ -30,5 +33,48 @@ class GetShaCommand extends Command
         $sha1 = exec('git rev-parse HEAD');
 
         return $sha1;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return bool
+     * @throws \Exception
+     */
+    private function validateArguments(InputInterface $input)
+    {
+        if (!$input->getArgument('repo')) {
+            throw new \Exception('The repository has not been defined');
+        }
+
+        if (!$input->getArgument('branch')) {
+            throw new \Exception('The branch has not been defined');
+        }
+
+        return true;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return bool
+     * @throws \Exception
+     */
+    private function validateOptions(InputInterface $input)
+    {
+        if ($input->getOption('service')) {
+            throw new \Exception('Sorry, only GitHub is supported at this time.');
+        }
+
+        return true;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @throws \Exception
+     */
+    private function validate(InputInterface $input)
+    {
+        $this->validateOptions($input);
+
+        $this->validateArguments($input);
     }
 }
